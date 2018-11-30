3683703a34a4b2bb7f092e8c270c8489821c083a
Matthew Liner <mattliner028@gmail.com>
2018-11-30 16:11:21 -0600

    start of project0

---
+++ PizzaStoreApp/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaStoreApp
+{
+    class User
+    {
+        public string firstName;
+        public string lastName;
+        public string location;
+
+        //2-hour time limit
+
+
+    }
+}
